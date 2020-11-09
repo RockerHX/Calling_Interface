dae014831e7913154523ed1f3d8689f6753243b8
RockerHX <rockerhx@gmail.com>
2020-11-09 11:14:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calling_Interface
+ Demo: https://www.youtube.com/watch?v=N5Cqyq3z5Jk
---
+++ android/app/src/main/kotlin/com/example/Calling_Interface/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Calling_Interface
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
